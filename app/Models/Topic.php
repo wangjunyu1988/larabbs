49b4eebedcb5ec7fca19629394f41f9f106db912
wang junyu <277272841@qq.com>
2020-08-19 21:23:46 +0000

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,7 +2,19 @@
 
 namespace App\Models;
 
+use App\Category;
+use App\User;
+
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
 }
---
+++ resources/views/topics/_sidebar.blade.php
@@ -0,0 +1,9 @@
+<div class="card ">
+  <div class="card-body">
+    右边导航栏
+  </div>
+</div><div class="card ">
+  <div class="card-body">
+    右边导航栏
+  </div>
+</div>
