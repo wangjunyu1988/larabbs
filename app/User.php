7c6b9bcc5ed1dc0e94928cafe9f6a73278b67a69
wang junyu <277272841@qq.com>
2020-08-20 10:57:29 +0000

    用户话题列表

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
+use App\Models\Topic;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
@@ -22,4 +23,8 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function topics(){
+        return $this->hasMany(Topic::class);
+    }
 }
