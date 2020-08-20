4921cfe4d8f5c3a5da990e26997ded8a9db119da
wang junyu <277272841@qq.com>
2020-08-20 10:50:41 +0000

    话题排序

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,10 +14,13 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+	public function index(Request $request,Topic $topic)
 	{
-		$topics = Topic::with('user','category')->paginate(30);
-		return view('topics.index', compact('topics'));
+		//$topics = Topic::with('user','category')->paginate(30);
+		$topics = $topic->withOrder($request->order)
+            ->with('user','category')
+            ->paginate(20);
+        return view('topics.index', compact('topics'));
 	}
 
     public function show(Topic $topic)
