5d887fa0821ae0d328564fa6106712190e096441
wang junyu <277272841@qq.com>
2020-08-22 01:24:26 +0000

    用户可以创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,13 @@
 <?php
+use Illuminate\Support\Str;
 
 function route_class(){
     return str_replace('.','-',Route::currentRouteName());
+}
+
+function make_excerpt($value,$length = 200){
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return Str::limit($excerpt, $length);
 }
 
 ?>
