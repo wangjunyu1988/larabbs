419e9f208e2495997d4613f45603d1d4cfeea886
wang junyu <277272841@qq.com>
2020-08-22 04:03:24 +0000

    用户可以上传话题图片

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -8,18 +8,31 @@ class ImageUploadHandler
     protected  $allowed_ext = ['png','jgp','gif','jpeg'];
 
     public function save($file,$folder,$file_prefix){
-        $folder_name = 'uploads/images/$folder/'.date('Ym/d',time());
-        $upload_path = public_path()."/".$folder_name;
-        $extension = strtolower($file->getClientOriginalExtension())?:'png';
-        $filename = $file_prefix.'_'.time().'_'.Str::random(10).'.'.$extension;
+        // 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
+        // 文件夹切割能让查找效率更高。
+        $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
 
-        if(!in_array($extension,$this->allowed_ext)){
+        // 文件具体存储的物理路径，`public_path()` 获取的是 `public` 文件夹的物理路径。
+        // 值如：/home/vagrant/Code/larabbs/public/uploads/images/avatars/201709/21/
+        $upload_path = public_path() . '/' . $folder_name;
+
+        // 获取文件的后缀名，因图片从剪贴板里黏贴时后缀名为空，所以此处确保后缀一直存在
+        $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
+
+        // 拼接文件名，加前缀是为了增加辨析度，前缀可以是相关数据模型的 ID
+        // 值如：1_1493521050_7BVc9v9ujP.png
+        $filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $extension;
+
+        // 如果上传的不是图片将终止操作
+        if ( ! in_array($extension, $this->allowed_ext)) {
             return false;
         }
-        $file->move($upload_path,$filename);
+
+        // 将图片移动到我们的目标存储路径中
+        $file->move($upload_path, $filename);
 
         return [
-            'page' => config('app.url').'/$folder_name/$filename'
+            'path' => config('app.url') . "/$folder_name/$filename"
         ];
     }
 }
