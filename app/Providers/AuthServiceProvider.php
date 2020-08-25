7f7fee75879c9ac08f7cc9be25c93f1cc9722d8b
wang junyu <277272841@qq.com>
2020-08-25 12:31:22 +0000

    站点权限部署

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,5 +31,10 @@ class AuthServiceProvider extends ServiceProvider
             return 'App\Policies\\'.class_basename($modelClass).'Policy';
         });
         //
+
+        \Horizon::auth(function ($request){
+            return \Auth::user()->hasRole('Founder');
+        });
+
     }
 }
